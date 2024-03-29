471e5f19c9bbce440a0ca43f5e84feb3c0493f21
sonoyasu <yasuhiro.sonobe.a@xeex.co.jp>
2024-03-29 16:21:39 +0900

    標準 日時表示 秒は非表示に変更

--- Java/Test.java
+++ Java/Test.java
@@ -8,7 +8,7 @@ class Test {
 
         // LogicA
         var now = new Date();
-        System.out.println((new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(now));        
+        System.out.println((new SimpleDateFormat("yyyy/MM/dd HH:mm")).format(now));        
         System.out.println("Hello World.");
 
         // LogicB
