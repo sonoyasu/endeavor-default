93e9d1eca44955c7cdf5e509d189017a9926d796
sonoyasu <yasuhiro.sonobe.a@xeex.co.jp>
2024-03-29 14:39:59 +0900

    標準Javaバージョン 初回コミット

---
+++ Java/Test.java
@@ -0,0 +1,19 @@
+
+class Test {
+    public static void main(String[] args)  {
+
+        System.out.println("標準Javaのテストです");
+
+        // LogicA
+        System.out.println("Hello World.");
+
+        // LogicB
+        if(true) {
+            System.out.println("LogicB");
+        }
+
+        return;
+    }
+    
+}
+
